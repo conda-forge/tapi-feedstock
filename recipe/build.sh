b848289cbc9725bcefce358cf31420832ebb85cb
Isuru Fernando <isuruf@gmail.com>
2019-09-25 14:06:42 -0500

    Try with clang 9

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,9 +7,13 @@ cmake \
     -G Ninja \
     -C $SRC_DIR/tapi/cmake/caches/apple-tapi.cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
-    -DCMAKE_C_COMPILER=clang \
-    -DCMAKE_CXX_COMPILER=clang++ \
-    -DCMAKE_ASM_COMPILER=clang \
+    -DCMAKE_C_COMPILER=$CC \
+    -DCMAKE_CXX_COMPILER=$CXX \
+    -DCMAKE_ASM_COMPILER=$CC \
+    -DCMAKE_LIBTOOL=$LIBTOOL \
+    -DCMAKE_AR=$AR \
+    -DCMAKE_NM=$NM \
+    -DCMAKE_INSTALL_NAME_TOOL=$INSTALL_NAME_TOOL \
     $SRC_DIR/llvm
 
 ninja install-distribution
