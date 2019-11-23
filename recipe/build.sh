1ea6745ae1215790872dda638a9545e60eb279cf
Isuru Fernando <isuruf@gmail.com>
2019-11-23 06:54:54 +0000

    Add linux support

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,16 @@
 mkdir build
 cd build
 
+if [[ "$target_platform" == osx* ]]; then
+    EXTRA_CMAKE_ARGS="-C $SRC_DIR/tapi/cmake/caches/apple-tapi.cmake $SRC_DIR/llvm"
+else
+    EXTRA_CMAKE_ARGS="$SRC_DIR/src/llvm"
+    # TODO: Fix this in a better way.
+    CXXFLAGS="$CXXFLAGS -I$SRC_DIR/src/llvm/projects/clang/include -I$SRC_DIR/build/projects/clang/include"
+fi
+
 cmake \
     -G Ninja \
-    -C $SRC_DIR/tapi/cmake/caches/apple-tapi.cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_C_COMPILER=$CC \
     -DCMAKE_CXX_COMPILER=$CXX \
@@ -16,6 +23,16 @@ cmake \
     -DCMAKE_RANLIB=$RANLIB \
     -DCMAKE_STRIP=$STRIP \
     -DCMAKE_INSTALL_NAME_TOOL=$INSTALL_NAME_TOOL \
-    $SRC_DIR/llvm
+    -DTAPI_REPOSITORY_STRING=tapi-${PKG_VERSION} \
+    -DLLVM_TARGETS_TO_BUILD=host \
+    -DTAPI_FULL_VERSION=10.0.0 \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    $EXTRA_CMAKE_ARGS
 
-ninja install-distribution
+if [[ "$target_platform" == osx* ]]; then
+    ninja install-distribution
+else
+    ninja clangBasic libtapi || ninja clangBasic libtapi
+    ninja install-libtapi install-tapi-headers
+fi
